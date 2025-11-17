44f8ab1e6db8c4d4230d9b0c43c4bce01dd0fc42
user <username@email.com>
2025-11-17 17:23:08 +0545

    app runs

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,10 +1,26 @@
-import "dart:ffi";
+import "dart:ffi" as ffi;
 
 import "package:ffi/ffi.dart";
 
 import "raylib.g.dart";
 
+// final ffi.Pointer<Color> myStructPtr = malloc<Color>()
+//   ..ref = createStructFn();
+
 void loadUI() {
-  InitWindow(600, 400, "myapp".toNativeUtf8().cast<Char>());
-  GetScreenHeight();
+  final ptr = calloc<Color>();
+  ptr.ref.r = 0;
+  ptr.ref.g = 228;
+  ptr.ref.b = 48;
+  ptr.ref.a = 255;
+  InitWindow(600, 400, "myapp".toNativeUtf8().cast<ffi.Char>());
+  //
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    // ClearBackground();
+    DrawText("this app".toNativeUtf8().cast<ffi.Char>(), 80, 50, 20, ptr.ref);
+    EndDrawing();
+  }
+  CloseWindow();
+  // GetScreenHeight();
 }
